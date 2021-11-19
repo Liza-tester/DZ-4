0ffe5ea63f29124c92c657b48d7b101b15ea4e29
Lisa <bear.bessie@gmail.com>
2021-11-19 19:07:27 +0300

    Initial commit

---
+++ src/test/java/guru/qa/DragAndDropTest.java
@@ -0,0 +1,44 @@
+package guru.qa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDropTest {
+
+    @BeforeAll
+    public static void beforeAllTests() {
+
+        //Открыть https://the-internet.herokuapp.com/drag_and_drop
+        Configuration.browserSize = "1900x1200";
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+    }
+
+    @Test
+    public void testDragAndDrop() {
+
+        //Перенести прямоугольник А на место В
+        $("#column-a").dragAndDropTo($("#column-b"));
+
+        //Проверить, что прямоугольники действительно поменялись
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+
+    @Test
+    public void testDragAndDropWithActions() {
+
+        //Перенести прямоугольник А на место В
+        actions().clickAndHold($("#column-a"))
+                .moveToElement($("#column-b"))
+                .build().perform();
+
+        //Проверить, что прямоугольники действительно поменялись
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+        sleep(1000);
+    }
+}
---
+++ src/test/java/guru/qa/GithubTest.java
@@ -0,0 +1,28 @@
+package guru.qa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GithubTest {
+
+    @Test
+    public void testGithub() {
+
+        //Открыть страницу Selenide в Github
+        Configuration.browserSize = "1900x1200";
+        open("https://github.com/selenide/selenide");
+
+        //Перейти в раздел Wiki проекта
+        $("#wiki-tab").click();
+
+        //Проверить, что в списке страниц (Pages) есть страница SoftAssertions
+        $(".markdown-body").$(byText("Soft assertions")).click();
+
+        //Открыть страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+        $(".markdown-body").shouldHave(text("com.codeborne.selenide.junit5.SoftAssertsExtension"));
+    }
+}
