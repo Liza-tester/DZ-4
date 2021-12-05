e88eb44539b87140642a8284470127b261d42f0e
sin <sinturik@gmail.com>
2021-12-05 21:45:57 +0600

    Add file, add class Dog

---
+++ src/test/java/guru/qa/Dog.java
@@ -0,0 +1,13 @@
+package guru.qa;
+
+public class Dog {
+    public void sayGav(){
+        System.out.println("GAV");
+    }
+
+    public static void main(String[] args) {
+        Dog Sharik = new Dog();
+        Sharik.sayGav();
+    }
+}
+
